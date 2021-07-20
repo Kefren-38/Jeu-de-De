474a05bad52f878ae4f9eeb76323148d9c296b0d
Kefren TORTOSA <kefrentortosa@gmail.com>
2021-07-20 17:10:59 +0200

    JS - Remise à zéro du dé lors du clique NEW GAME

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(() => {
     let newGame = $('#btn-NEWGAME')
     let rollDice = $('#rollDice')
     let hold = $('#hold')
+
     let images = ["images/de-1.png",
     "images/de-2.png",
     "images/de-3.png",
@@ -18,15 +19,30 @@ $(() => {
         $('#globalScoreJ1, #globalScoreJ2, #roundScoreJ1, roundScoreJ2').text(0);
 
         $('.namePlayer1, #globalScoreJ1').css('font-weight', 'bold');
+
+        $('#dice').attr("src", images[0]);
     })
 
     //ROLL DICE
-    //LORS DU CLIC, UN DE S'AFFICHE
+    //LORS DU CLIC, UN DE S'AFFICHE ALEATOIREMENT
     $('#rollDice').click( function () {
         let dice = Math.floor(Math.random()*6);
         console.log(dice);
         $('#dice').attr("src", images[dice]);
+
+        //AFFICHE LE NOMBRE DU DE DANS CURRENT
+        let current = $('#roundScoreJ1').text(dice +1)
+        
+
+        //PREND LE SCORE CURRENT ET L'ADDITIONNE AU TOTAL
+        //let current = dice;
+        //console.log(current +1)
+        //console.log(dice + score)
+    
+        if(dice === 0) {
+            $('#roundScoreJ1').text(10);
+        }
     })
 
-    console.log(dice)
+    
 });
