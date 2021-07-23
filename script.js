12eb149dae82dbf0fc1207d78bbd8ff694656677
Kefren TORTOSA <kefrentortosa@gmail.com>
2021-07-23 13:49:34 +0200

    JS - Ajout de commentaire + addition des points OK

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(() => {
     let newGame = $('#btn-NEWGAME')
     let rollDice = $('#rollDice')
     let hold = $('#hold')
+    let roundScoreJ1 = $('#roundScoreJ1');
+    let roundScoreJ2 = $('#roundScoreJ2');
 
     let images = ["images/de-1.png",
     "images/de-2.png",
@@ -14,35 +16,77 @@ $(() => {
     
     
     //NEW GAME 
-    //LORS DU CLIC, TOUS LES SCORES REVIENNENT A 0
+    //Retour des scores à zéro
     $('#btn-NEWGAME').click( function () {
+        
+        //1. Retour des scores à zéro
         $('#globalScoreJ1, #globalScoreJ2, #roundScoreJ1, roundScoreJ2').text(0);
 
-        $('.namePlayer1, #globalScoreJ1').css('font-weight', 'bold');
+        //2. Ajoute la class "active" au joueur 1
+        $('.namePlayer1, #globalScoreJ1').addClass('active');
 
+        //3. Supprime la class "active" au joueur 2
+        $('.namePlayer2, #globalScoreJ2').removeClass('active');
+
+        //4. Affiche le dé par défault: le 1
         $('#dice').attr("src", images[0]);
+
     })
 
+
     //ROLL DICE
-    //LORS DU CLIC, UN DE S'AFFICHE ALEATOIREMENT
+    //Lancé de dé aléatoire
     $('#rollDice').click( function () {
+ 
+        //1. Créa d'un let générant un nombre random de 1 à 6
         let dice = Math.floor(Math.random()*6);
-        console.log(dice);
+        
+        //2. Affiche dans la console
+        //Attribue l'image correspondante
+        console.log(dice+1);
         $('#dice').attr("src", images[dice]);
 
-        //AFFICHE LE NOMBRE DU DE DANS CURRENT
-        let current = $('#roundScoreJ1').text(dice +1)
-        
-
-        //PREND LE SCORE CURRENT ET L'ADDITIONNE AU TOTAL
-        //let current = dice;
-        //console.log(current +1)
-        //console.log(dice + score)
-    
+        //3. Affiche le résultat du dé dans "current" du joueur1
+        $('#roundScoreJ1').text(dice+1);
+      
+        //4. Si le résultat est 1, le joueur 1 perd ses points 
         if(dice === 0) {
-            $('#roundScoreJ1').text(10);
+            $('#roundScoreJ1').text(0);
+            nextPlayer();
         }
     })
 
-    
+    //function scorej1 () {
+    //    let score = 
+    //}
+
+    //NEXTPLAYER
+    //Fonction de changement de joueur 
+    function nextPlayer () {
+        if($('#globalScoreJ1, .namePlayer1').css('active') !== true) {
+            $('#globalScoreJ1, .namePlayer1').removeClass('active');
+            $('#globalScoreJ2, .namePlayer2').addClass('active');
+        } else {
+            $('#globalScoreJ1, .namePlayer1').addClass('active');
+            $('#globalScoreJ2, .namePlayer2').removeClass('active');
+        }
+    }
+
+
+    $('#hold').click( function () {
+        let scoreRound = $('#roundScoreJ1').text()
+        let scoreGlobal = $('#globalScoreJ1').text()
+        let score = parseInt(scoreRound) + parseInt(scoreGlobal)
+        
+        if($('#globalScoreJ1, .namePlayer1').css('active') !== true){
+            $('#globalScoreJ1').text(score);
+            nextPlayer()
+        } else { 
+            $('#globalScoreJ2').text($('#roundScoreJ1').text());
+            nextPlayer()    
+        }
+
+    })
+//
+
 });
