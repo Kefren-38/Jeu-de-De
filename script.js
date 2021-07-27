b2877b47e9c99ada94ff6d93d1fc2726463b46be
Kefren TORTOSA <kefrentortosa@gmail.com>
2021-07-27 12:18:34 +0200

    JS - Ajout de commentaire et suppression des let inutiles

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 // jQuery
 $(() => {
-    let newGame = $('#btn-NEWGAME')
-    let rollDice = $('#rollDice')
-    let hold = $('#hold')
-    let roundScoreJ1 = $('#roundScoreJ1');
-    let roundScoreJ2 = $('#roundScoreJ2');
-
+    
     let images = ["images/de-1.png",
     "images/de-2.png",
     "images/de-3.png",
@@ -14,10 +9,8 @@ $(() => {
     "images/de-6.png"];
     let dice = $('#dice');
     
-    
-    //NEW GAME 
-    //Retour des scores à zéro
 
+    //Fonction d'initialisation de la partie
     function init () {
         //1. Apparition et retour des scores à zéro
         $('#globalScoreJ1, #globalScoreJ2, #roundScoreJ1, roundScoreJ2, .namePlayer1, .namePlayer2').delay(500).fadeIn(700).text(0);
@@ -37,6 +30,9 @@ $(() => {
         $('.namePlayer2').text('PLAYER 2').removeClass('winner')
     }
 
+
+    //NEW GAME
+    //Au clic, initialisation de la partie & ajout de la class 'active' au joueur1
     $('#btn-NEWGAME').click( function () {
         init ()
         $('.namePlayer1, #globalScoreJ1').addClass('active'); 
@@ -57,7 +53,7 @@ $(() => {
         $('#dice').attr("src", images[dice]);
 
 
-        //3. Affiche le résultat du dé dans "current" du joueur concerné
+        //3. Affiche et additionne le résultat du dé dans "current" du joueur ayant la class 'active'
         if($('.namePlayer1').hasClass('active') == true) {
             let roundScore = $('#roundScoreJ1').text()
             let totalScoreRound = parseInt(dice+1) + parseInt(roundScore)
@@ -70,9 +66,7 @@ $(() => {
             
             $('#roundScoreJ2').text(totalScoreRoundJ2)
         }
-   
-
-        
+    
         //4. Si le résultat du lancé est 1, le joueur perd ses points
         if(dice === 0) {
             nextPlayer();
@@ -93,9 +87,7 @@ $(() => {
             $('#globalScoreJ2, .namePlayer2').removeClass('active');
             $('#roundScoreJ2').text(0);
             $('#globalScoreJ1, .namePlayer1').addClass('active');
-        }
-        $('#cardJ1, #cardJ2').addClass('card')
-        
+        }  
     }
 
 
@@ -106,7 +98,7 @@ $(() => {
         let scoreGlobal = $('#globalScoreJ1').text()
         let score = parseInt(scoreRound) + parseInt(scoreGlobal)
 
-        //1. let pour le joueur 2
+        //2. let pour le joueur 2
         let scoreRoundJ2 = $('#roundScoreJ2').text()
         let scoreGlobalJ2 = $('#globalScoreJ2').text()
         let scoreJ2 = parseInt(scoreRoundJ2) + parseInt(scoreGlobalJ2)
@@ -135,34 +127,45 @@ $(() => {
         $('#dice').attr("src", images[0]);
     })
  
+
+    //Fonction de fin de jeu
+    //1. Permet de cacher les boutons RollDice et Hold
     function endGame () {
         $('#rollDice, #hold').fadeOut(1000);    
     }
 
     
     //Animations survole de la souris
+    //Survole HOLD
     $('#hold').hover(function () {
         $(this).addClass('active');
     }, function () {
         $(this).removeClass('active');
     })
 
+    //Survole New Game
     $('h1').hover(function () {
         $(this).addClass('active');
     }, function () {
         $(this).removeClass('active');
     })
     
+    //Survole Hover
     $('#rollDice').hover(function () {
         $(this).addClass('active');
     }, function () {
         $(this).removeClass('active');
     })
 
+
+
+    //Apparition des règles lors du clique sur le logo "i"
     $('.bi').click(function () {
+        //1. Si regles est visible, alors les cachées
         if ($('.regles').is(':visible')) {
             $('.regles').fadeOut(300);
             $('.bi').css('color', 'rgb(39, 39, 39)')
+        //2. Sinon, les montrer
         } else {
             $('.regles').fadeIn(1000); 
             $('.bi').css('color', '#0d6efd')
